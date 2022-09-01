50862303b46a29869e372e1c36a3988d4fc939f1
Alexei Beuno H <67053046+Alexeibeuno@users.noreply.github.com>
2022-09-01 17:18:31 +0530

    Add files via upload

---
+++ Leaflet-Basic-master/Basic/data/line.js
@@ -0,0 +1,1 @@
+var lineJson = { "type": "FeatureCollection", "features": [{ "type": "Feature", "properties": {}, "geometry": { "type": "LineString", "coordinates": [[42.2416375,-21.0301094],[26.6671059,88.4188101],[19.2636403,73.0735726]] } }] }
